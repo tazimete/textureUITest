5f629211bd60a67b65a5a802a0ae358955e938ba
tazimete <tazimete@gmail.com>
2022-06-03 18:58:54 +0600

    Init project

---
+++ textureUITest/textureUITest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  textureUITest
+//
+//  Created by AGM Tazim on 6/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
